6d5f8fb88a9392032e48726ce3c22857395d934b
BlocksDev <blocksdev.pro@gmail.com>
2023-12-02 21:36:19 +0530

    Update v2

--- helper.py
+++ helper.py
@@ -1,6 +1,6 @@
 import time
 import webbrowser
-from pyautogui import click, hotkey, press, size, typewrite
+from pyautogui import click, hotkey, press, size, typewrite, screenshot
 
 WIDTH, HEIGHT = size()
 
@@ -9,6 +9,14 @@ class whatsapp:
     def __init__(self):
         self.is_working = None
         pass
+    
+    def gen_login_qr(self):
+        self.is_working = True
+        webbrowser.open('https://web.whatsapp.com/')
+        image = screenshot()
+        self.is_working = False
+        return image
+    
     def open_chat(self, receiver: str):
         webbrowser.open(f"https://web.whatsapp.com/accept?code={receiver}")
         time.sleep(7)
@@ -26,6 +34,7 @@ class whatsapp:
         self.open_chat(receiver)
         self.enter_message(message)
         press("enter")
+        hotkey("ctrl", "w")
         self.is_working = False
         
         
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from telebot.types import Message
 whatsapp = whatsapp()
 
 bot = TeleBot(config.TELEGRAM_BOT_TOKEN)
+print(bot.user.username)
 
 def message_handler(message: Message):
     while whatsapp.is_working:
@@ -17,4 +18,14 @@ def message_handler(message: Message):
 bot.register_message_handler(message_handler, func=lambda message: (str(message.chat.id) == config.TELEGRAM_CHAT_ID) and message.text)
 
 if __name__ == "__main__":
+    while True:
+        logged_in = input('You have logged in ? True/False : ')
+        if logged_in in ['True', 'False']:
+            if logged_in == 'False':
+                bot.send_photo(config.TELEGRAM_ADMIN_ID, whatsapp.gen_login_qr())
+            else:
+                break
+        else:
+            continue
+    
     bot.infinity_polling()
